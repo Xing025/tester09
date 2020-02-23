2beed25ffc148ed1ec0a97ea65bb5ac0651e02d2
Xing025 <1271529159@qq.com>
2020-02-23 20:59:56 +0800

    twst02

---
+++ package09/package02/02_setBrowwer.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+from selenium import webdriver
+import unittest
+import time
+
+driver=webdriver.Firefox()
+driver.get('http://www.testertechnology.com/forum')
+#通过css_selector（）定位控件：精品贴 并点击
+driver.find_element_by_css_selector("span#postCata>a:nth-child(2)").click()
+time.sleep(5)
+time.sleep(2)
+driver.quit()
